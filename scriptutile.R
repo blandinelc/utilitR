206091276a1f256209359979a95975dca03ddbec
Blandine Le Cain <blandine.lecain@gmail.com>
2020-01-27 14:46:06 +0100

    Import de multiples csv

--- scriptutile.R
+++ scriptutile.R
@@ -1,16 +1,25 @@
 #### Importer plusieurs fichiers à la fois ####
 
+#Traiter plusieurs csv en une fois
+csv <- list.files(path = "./dossier_avec_csv/", 
+                  pattern = ".csv", 
+                  full.names = TRUE) %>% #full.names permet d'inclure le dossier si pas le même que le wd
+  #Ajouter le nom des csv
+  setNames(.,              #Définit le pattern (modèle) à utiliser
+           sub("\\.csv$",  #Texte à remplacer via fonction sub()
+               "",         #Texte de remplacement
+               basename(.))) #Les valeurs caractères à utiliser, ici le nom de base du pattern
+
+tous <- lapply(csv, fread)
 
 #Ajouter le nom des donnees à part, sans réécrire le nom
 tous <- setNames(lapply(csv, fread), csv)
 
 #Grouper et exporter
-substance_bzh <- bind_rows(tous, .id = "csv")
-fwrite(substance_bzh, file = "substance_bzh_20132018.csv", sep = ";")
+df <- bind_rows(tous, .id = "csv")
+fwrite(df, file = "substance_bzh_20132018.csv", sep = ";")
 
 #Extraire les différents ID
-substance_bzh %>% 
+df %>% 
   distinct(csv)
 
-#Afficher une partie d'un dataset
-slice(airports, 1:5)
